54deba554d74d4808e9f8704c5d1514dc3a9315e
nonsodaniel <nonsodaniel07@gmail.com>
2021-03-12 01:05:02 +0100

    Added webcam and camera properties

--- src/App.js
+++ src/App.js
@@ -1,9 +1,81 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import * as tf from "@tensorflow/tfjs";
+import Webcam from "react-webcam";
+import { ObjectDetection } from "@tensorflow-models/coco-ssd";
+import * as cocossd from "@tensorflow-models/coco-ssd";
 
-const App = () => {
+
+const App = () => { 
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  const runCoco = async () => {
+    const net = await cocossd.load();
+    setInterval(() => {
+      detect(net);
+    }, 10);
+  };
+
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      //get video properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      const obj = await net.detect(video);
+      console.log(obj);
+
+      //draw the mesh
+      const ctx = canvasRef.current.getContent("2d");
+    }
+  };
+
   useEffect(() => {}, []);
 
-  return <div className="app">Hello</div>;
+  return (
+    <div className="app">
+      <header className="app-header">
+        <Webcam
+          ref={webcamRef}
+          muted={true}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            textAlign: "center",
+            width: 640,
+            height: 480,
+          }}
+        />
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 8,
+            width: 640,
+            height: 480,
+          }}
+        />
+      </header>
+    </div>
+  );
 };
 
 export default App;
